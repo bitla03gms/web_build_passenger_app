d03574c70fee51141c859e6db5377621b5bccf9a
Govind <govindmane@bitlasoft.com>
2026-05-15 18:31:47 +0530

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,7 +17,7 @@ const RESOURCES = {"canvaskit/skwasm_heavy.js": "740d43a6b8240ef9e23eed8c48840da
 "canvaskit/skwasm.js": "8060d46e9a4901ca9991edd3a26be4f0",
 "index.html": "ff3679fde52c8a827624e364d9858481",
 "/": "ff3679fde52c8a827624e364d9858481",
-"flutter_bootstrap.js": "267f9a5ac0ef7ce59a1cd38d1f2f63c5",
+"flutter_bootstrap.js": "62bf0b34ccef6f202b13225ed0e7aac4",
 "404.html": "033c8173bb1753acea0a901ec61332e6",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
@@ -116,7 +116,7 @@ const RESOURCES = {"canvaskit/skwasm_heavy.js": "740d43a6b8240ef9e23eed8c48840da
 "manifest.json": "b6e884c2aada6f3b1ddde31d53f7ae9e",
 "version.json": "b64ed79850ff1b838810558fb8c60216",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "dce067f0c57cae6a2ad41b4f66d02cc3",
+"main.dart.js": "c58659cde284b00b6e864fc5785a882b",
 "payment_loading.html": "e9c97b58ac5b08c19080c8f039dfee38"};
 // The application shell files that are downloaded before a service worker can
 // start.
