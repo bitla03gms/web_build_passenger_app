e43374497685fabde2fbd0c0a01f5a9995ac63c3
Govind <govindmane@bitlasoft.com>
2026-06-11 11:27:51 +0530

    ios live issue checking

---
+++ coi-serviceworker.js
@@ -0,0 +1,31 @@
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (event) =>
+  event.waitUntil(self.clients.claim())
+);
+
+self.addEventListener("fetch", function (event) {
+  if (
+    event.request.cache === "only-if-cached" &&
+    event.request.mode !== "same-origin"
+  ) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        const newHeaders = new Headers(response.headers);
+        newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+        newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+
+        return new Response(response.body, {
+          status: response.status,
+          statusText: response.statusText,
+          headers: newHeaders,
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      })
+  );
+});
